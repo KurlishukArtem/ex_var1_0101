37a2010e361a5c9c6db82f954416380e0befe0e3
Daria <kobelevadara210@mail.com>
2025-02-28 08:39:01 +0500

    Добавьте файлы проекта.

---
+++ Contexts/Config/Config.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ex_var1.Contexts.Config
+{
+    public class Config
+    {
+        public static readonly string connect = "";
+        public MySqlServerVersion serverVersion = new MySqlServerVersion(new Version(8,0,11));
+    }
+}
